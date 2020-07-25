80486c547238bace718af21bf9faec88755595e8
Clusternite <sterregovaert@gmail.com>
2020-07-25 20:18:17 +0200

    changes

--- js/problemsSolutions.js
+++ js/problemsSolutions.js
@@ -1,44 +1,48 @@
-let src = document.getElementById("images");
-const pageNames = ["primary", "rental", "multipurpose", "barn", "outside"];
-let len = 85;
-const starting = () => {
-  let urlname = window.location.pathname.slice(0, -5);
-  if (urlname == "/allimages") {
-    pageNames.forEach(pageName => {
-      addImagesToPageFromURl(`/${pageName}`)
-    })
-  } else
-    addImagesToPageFromURl(urlname);
-}
+// let src = document.getElementById("images");
+// const pageNames = ["primary", "rental", "multipurpose", "barn", "outside"];
+// let len = 85;
+// const starting = () => {
+//   let urlname = window.location.pathname.slice(0, -5);
+//   if (urlname == "/allimages") {
+//     pageNames.forEach(pageName => {
+//       addImagesToPageFromURl(`/${pageName}`)
+//     })
+//   } else
+//     addImagesToPageFromURl(urlname);
+// }
 
-function addImagesToPageFromURl(urlname) {
-  for (let i = 1; i <= len; i++) {
-    let img = new Image();
-    img.src = `./images${urlname}${urlname} (${i}).jpg`;
-    img.onload = () => {
-      if (img.width !== 0) {
-        img.setAttribute("class", 'img-fluid');
-        img.alt = 'Image';
+// function addImagesToPageFromURl(urlname) {
+//   for (let i = 1; i <= len; i++) {
+//     let img = new Image();
+//     img.src = `./images${urlname}${urlname} (${i}).jpg`;
+//     img.onload = () => {
+//       if (img.width !== 0) {
+//         img.setAttribute("class", 'img-fluid');
+//         img.alt = 'Image';
 
-        let a = document.createElement('a');
-        a.href = `./images${urlname}${urlname} (${i}).jpg`;
+//         let a = document.createElement('a');
+//         a.href = `./images${urlname}${urlname} (${i}).jpg`;
 
-        a.setAttribute("class", "d-block figure");
-        a.setAttribute("data-fancybox", "gallery");
-        a.setAttribute("data-width", "500");
-        // a.setAttribute("data-thumb", `./images${urlname}${urlname} (${i}).jpg`)
-        a.setAttribute("data-caption", `${urlname}`);
-        a.appendChild(img);
+//         a.setAttribute("class", "d-block figure");
+//         a.setAttribute("data-fancybox", "gallery");
+//         a.setAttribute("data-width", "500");
+//         // a.setAttribute("data-thumb", `./images${urlname}${urlname} (${i}).jpg`)
+//         a.setAttribute("data-caption", `${urlname}`);
+//         a.appendChild(img);
 
-        let div = document.createElement('div');
-        div.setAttribute("class", "col-lg-4 col-md-6 mb-4 mb-lg-0 post-entry");
-        div.appendChild(a);
+//         let div = document.createElement('div');
+//         div.setAttribute("class", "col-lg-4 col-md-6 mb-4 mb-lg-0 post-entry");
+//         div.appendChild(a);
 
-        src.appendChild(div);
-      }
-    }
-  }
-}
+//         src.appendChild(div);
+//       }
+//     }
+//   }
+// }
+
+
+
+
 //video.js
 document.getElementById("introBtn").addEventListener("click", () => {
   document.getElementById("vidStart").click();
@@ -53,7 +57,19 @@ const init = () => {
   });
 }
 
+//put everything between src in href
+const sourceAndLinkEqualizer = () => {
+  let imgArray = document.getElementsByTagName('img');
+  for (i = 0; i < imgArray.length; i++) {
+    let imgTag = imgArray[i];
+    let sourceAttribute = imgTag.getAttribute('src');
+    imgTag.setAttribute('href', sourceAttribute);
+  }
+};
+
+// LOAD
 window.onload = () => {
-  init;
-  starting;
+  init();
+  // starting;
+  sourceAndLinkEqualizer();
 }
