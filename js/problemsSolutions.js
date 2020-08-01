5931cc05cd3bc596928ce0f4d936ba0ee69cd9a8
Clusternite <sterregovaert@gmail.com>
2020-08-01 19:39:42 +0200

    js

--- js/problemsSolutions.js
+++ js/problemsSolutions.js
@@ -1,3 +1,160 @@
+// -------------------------- Lazy loading images  -------------------------- 
+document.addEventListener("DOMContentLoaded", function () {
+  let imgArray = document.getElementsByTagName('img');
+  for (i = 0; i < imgArray.length; i++) {
+    let imgTag = imgArray[i];
+    let attribute = imgTag.getAttribute('class');
+    imgTag.setAttribute('class', `${attribute} lazy`);
+  }
+  var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadThrottleTimeout;
+
+  function lazyload() {
+    if (lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function () {
+      var scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function (img) {
+        if (img.offsetTop < (window.innerHeight + scrollTop)) {
+          img.src = img.dataset.src;
+          img.classList.remove('lazy');
+        }
+      });
+      if (lazyloadImages.length == 0) {
+        document.removeEventListener("scroll", lazyload);
+        window.removeEventListener("resize", lazyload);
+        window.removeEventListener("orientationChange", lazyload);
+      }
+    }, 20);
+  }
+
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
+
+
+// -------------------------- Change between languages -------------------------- 
+const displayElementsInMenu = (displayFrench, displayDutch) => {
+  const arrayFrench = document.getElementsByClassName("nav-frech")
+  const arrayDutch = document.getElementsByClassName("nav-dutch")
+  for (let i = 0; i < arrayFrench.length; i++) {
+    let elemDutch = arrayDutch[i];
+    elemDutch.style.display = displayDutch;
+    let elemFrench = arrayFrench[i];
+    elemFrench.style.display = displayFrench;
+  }
+}
+
+const languageDutch = () => {
+  document.getElementsByClassName("french")[0].setAttribute("style", "display: none;");
+  document.getElementsByClassName("dutch")[0].setAttribute("style", "display: contents;");
+  localStorage.setItem("vierkantshoeve_language", "dutch");
+
+  displayElementsInMenu("none", "");
+}
+
+const languageFrench = () => {
+  document.getElementsByClassName("french")[0].setAttribute("style", "display: contents;");
+  document.getElementsByClassName("dutch")[0].setAttribute("style", "display: none;");
+  localStorage.setItem("vierkantshoeve_language", "french");
+
+  displayElementsInMenu("", "none");
+}
+
+// dutch button when pressed
+document.getElementById("dutchBtn").addEventListener("click", languageDutch);
+
+// french button when pressed
+document.getElementById("frenchBtn").addEventListener("click", languageFrench);
+
+
+
+// -------------------------- video.js -------------------------- 
+document.getElementsByClassName("introBtn")[0].addEventListener("click", () => {
+  document.getElementsByClassName("vidStart")[0].click();
+});
+
+// -------------------------- nav.js -------------------------- 
+const init = () => {
+  [...document.getElementsByClassName("childnavlink")].forEach((linkElem) => {
+    linkElem.addEventListener("click", () => {
+      window.location = `${linkElem.href}`
+    });
+  });
+}
+
+// -------------------------- change everything from src to href -------------------------- 
+const sourceAndLinkEqualizer = () => {
+  let imgArray = document.getElementsByTagName('img');
+  for (i = 0; i < imgArray.length; i++) {
+    let imgTag = imgArray[i];
+    let sourceAttribute = imgTag.getAttribute('src');
+    imgTag.setAttribute('href', sourceAttribute);
+  }
+};
+// -------------------------- Local storage -------------------------- 
+const setLanguage = () => {
+  const language = localStorage.getItem("vierkantshoeve_language");
+  switch (language) {
+    case "dutch":
+      languageDutch();
+      break;
+    case "french":
+      languageFrench();
+      break;
+    default:
+      languageDutch();
+      break;
+  }
+}
+
+//  -------------------------- LOAD -------------------------- 
+window.onload = () => {
+  init();
+  sourceAndLinkEqualizer();
+  setLanguage();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let src = document.getElementById("images");
 // const pageNames = ["primary", "rental", "multipurpose", "barn", "outside"];
 // let len = 85;
@@ -39,85 +196,3 @@
 //     }
 //   }
 // }
-
-const displayElementsInMenu = (displayFrench, displayDutch) => {
-  const arrayFrench = document.getElementsByClassName("nav-frech")
-  const arrayDutch = document.getElementsByClassName("nav-dutch")
-  for (let i = 0; i < arrayFrench.length; i++) {
-    let elemDutch = arrayDutch[i];
-    elemDutch.style.display = displayDutch;
-    let elemFrench = arrayFrench[i];
-    elemFrench.style.display = displayFrench;
-  }
-}
-
-const languageDutch = () => {
-  document.getElementsByClassName("french")[0].setAttribute("style", "display: none;");
-  document.getElementsByClassName("dutch")[0].setAttribute("style", "display: contents;");
-  localStorage.setItem("vierkantshoeve_language", "dutch");
-
-  displayElementsInMenu("none", "");
-}
-
-const languageFrench = () => {
-  document.getElementsByClassName("french")[0].setAttribute("style", "display: contents;");
-  document.getElementsByClassName("dutch")[0].setAttribute("style", "display: none;");
-  localStorage.setItem("vierkantshoeve_language", "french");
-
-  displayElementsInMenu("", "none");
-}
-
-// dutch button when pressed
-document.getElementById("dutchBtn").addEventListener("click", languageDutch);
-
-// french button when pressed
-document.getElementById("frenchBtn").addEventListener("click", languageFrench);
-
-
-
-//video.js
-document.getElementsByClassName("introBtn")[0].addEventListener("click", () => {
-  document.getElementsByClassName("vidStart")[0].click();
-});
-
-//nav.js
-const init = () => {
-  [...document.getElementsByClassName("childnavlink")].forEach((linkElem) => {
-    linkElem.addEventListener("click", () => {
-      window.location = `${linkElem.href}`
-    });
-  });
-}
-
-//put everything between src in href
-const sourceAndLinkEqualizer = () => {
-  let imgArray = document.getElementsByTagName('img');
-  for (i = 0; i < imgArray.length; i++) {
-    let imgTag = imgArray[i];
-    let sourceAttribute = imgTag.getAttribute('src');
-    imgTag.setAttribute('href', sourceAttribute);
-  }
-};
-
-const setLanguage = () => {
-  const language = localStorage.getItem("vierkantshoeve_language");
-  switch (language) {
-    case "dutch":
-      languageDutch();
-      break;
-    case "french":
-      languageFrench();
-      break;
-    default:
-      languageDutch();
-      break;
-  }
-}
-
-// LOAD
-window.onload = () => {
-  init();
-  // starting;
-  sourceAndLinkEqualizer();
-  setLanguage();
-}
