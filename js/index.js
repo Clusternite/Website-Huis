03fa24ceb5515339780a9d7dc5bd9b5de999b94b
Clusternite <sterregovaert@gmail.com>
2020-10-28 21:49:18 +0100

    fixed back to top button and changed js name

--- js/index.js
+++ js/index.js
@@ -116,6 +116,25 @@ const setLanguage = () => {
       break;
   }
 };
+//  -------------------------- Back to Top button --------------------------
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 //  -------------------------- LOAD --------------------------
 window.onload = () => {
@@ -123,6 +142,8 @@ window.onload = () => {
   sourceAndLinkEqualizer();
   setLanguage();
 };
+
+
 
 // let src = document.getElementById("images");
 // const pageNames = ["primary", "rental", "multipurpose", "barn", "outside"];
